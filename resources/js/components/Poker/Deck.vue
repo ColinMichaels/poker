10aff7d8bdbcccf7212fbaacc762a7efae924284
Colin Michaels <colin@colinmichaels.com>
2019-11-21 16:13:21 -0500

    Adding tag manager and GA tagging to main view

--- resources/js/components/Poker/Deck.vue
+++ resources/js/components/Poker/Deck.vue
@@ -1,30 +1,25 @@
 <template>
-    <div>
-        <transition-group name="shuffleDeck" tag="div" class="flex flex-wrap" id="deck" :class="{is_flipped: !is_flipped}" ref="deck" >
-                <card
-                    :ref="card.name"
-                    v-for="card in cards"
-                    :name="card.name"
-                    :key="card.name"
-                    :is_flipped="is_flipped"
-                    v-on:card-clicked="flip"
-                ></card>
-        </transition-group>
+    <div id="deck" ref="deck" :class="{is_flipped: !is_flipped}"  class="flex flex-wrap shuffle" >
+            <card
+                :ref="card.name"
+                v-for="card in cards"
+                :name="card.name"
+                :key="card.name"
+                :is_flipped="is_flipped"
+                v-on:card-clicked="flip"
+            ></card>
     </div>
 </template>
 
 <script>
     import Game from "@/plugins/game/GamePlugin";
-    import {gsap, CSSPlugin, TweenMax} from "gsap/all";
-    gsap.registerPlugin(CSSPlugin);
-    import Draggable from "gsap/Draggable";
     import Card from "./Card";
 
-    const initData = ()=>({
+    const initData = () => ({
         is_flipped: false,
-        cards: Game.store().cards.slice(0,5),
-        play_hand : [],
-        position: Math.floor(Math.random() * (52 - 1 ) +1),
+        cards: Game.store().cards.slice(0, 5),
+        play_hand: [],
+        position: Math.floor(Math.random() * (52 - 1) + 1),
         selected: null
     });
 
@@ -34,8 +29,8 @@
             return {
                 is_flipped: false,
                 cards: initData().cards,
-                play_hand : [],
-                position: Math.floor(Math.random() * (52 - 1 ) +1),
+                play_hand: [],
+                position: Math.floor(Math.random() * (52 - 1) + 1),
                 selected: null
             }
         },
@@ -43,49 +38,44 @@
             Card
         },
         methods: {
-            remove(card){
+            remove(card) {
                 Game.sound('/Poker/sounds/card.mp3');
                 //_.slice(this.cards, _.findIndex(this.cards,  card.name ));
             },
             deal(num_per) {
                 this.resetDeck();
-                this.is_flipped= false;
             },
             draw(card) {
-               let deck_card = this.cards[this.position];
+                let deck_card = this.cards[this.position];
                 Game.broadcast('card.start', deck_card.name);
-               if(deck_card.name === card){
-                   Game.broadcast('game.end');
-                   Game.sound('/Poker/sounds/tada.mp3');
-                   if(confirm(  'you found the card it was the '+ deck_card.description + '\n would you like to play again?'))
-                        this.resetDeck();
-
-               }
             },
-            resetDeck(){
-                Object.assign(this.$data , initData());
+            resetDeck() {
+                Object.assign(this.$data, initData());
+                this.flipAll(true);
+                this.shuffle();
+                this.flipAll(false);
 
             },
             flip(card) {
                 card.flipped = !card.flipped;
                 Game.sound('/Poker/sounds/card.mp3');
             },
-            flipAll(dir){
-                this.is_flipped = !this.is_flipped;
-                this.cards.forEach((card)=>{
-                     let cur_card = this.$refs[card.name][0];
-                     if(cur_card !== undefined){
-                         cur_card.flipped = this.is_flipped;
-                     }
+            flipAll(dir = null) {
+                this.is_flipped = (dir) ? dir : !this.is_flipped;
+                this.cards.forEach((card) => {
+                    let cur_card = this.$refs[card.name][0];
+                    if (cur_card !== undefined) {
+                        cur_card.flipped = (dir) ? dir : this.is_flipped;
+                    }
                 });
                 Game.sound('/Poker/sounds/card.mp3');
 
             },
             shuffle() {
-                this.cards =   _.shuffle(this.cards);
+                this.cards = _.shuffle(this.cards);
                 Game.sound('/Poker/sounds/shuffle.mp3');
             },
-            updateCard(card){
+            updateCard(card) {
                 this.draw(card);
             }
         },
@@ -94,8 +84,6 @@
             Game.listen('deck.deal', this.deal);
             Game.listen('deck.flip', this.flipAll);
             Game.listen('deck.reset', this.resetDeck);
-        }, updated(){
-
         }
     }
 </script>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,30 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Google Tag Manager -->
+    <script>(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', '{{env('GOOLE_TAGMANAGER_ID')}}');</script>
+    <!-- End Google Tag Manager -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <script src="{{ mix('js/app.js') }}" defer></script>
     @routes
 </head>
 <body class="h-screen antialiased leading-none">
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id={{env('GOOLE_TAGMANAGER_ID')}}"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
 
 @inertia
 
